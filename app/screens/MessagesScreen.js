7ca19ddf0edd29ed9daa1ebd2895ee7bd11bf03e
Maziar <maziar@Maziars-MBP.attlocal.net>
2024-04-21 22:27:57 -0400

    DeletedMessage

--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import { FlatList, StyleSheet } from 'react-native';
+import React, {useState} from 'react';
+import { FlatList, StyleSheet, View } from 'react-native';
 
 import Screen from '../components/Screen';
 import ListItem from '../components/ListItem';
 import ListItemSeparator from '../components/ListItemSeparator';
+import {Swipeable} from 'react-native-gesture-handler/Swipeable';
 
-const messages= [
+import ListItemDeleteAction from '../components/ListItemDeleteAction';
+const InitialMessages= [
     {
         id: 1,
         title: 'T1',
@@ -20,24 +22,67 @@ const messages= [
     }
 ]
 
+
+
 function MessagesScreen(props) {
+
+    const [messages, setMessages] = useState(InitialMessages);
+    const handelDelete = message => {
+    setMessages(messages.filter (m => m.id !== message.id ));
+    }
+
     return (
+        <Screen>
+            <FlatList
+            data={messages}
+            keyExtractor={message => message.id.toString()}
+            renderItem={({item}) => 
+                <ListItem
+                 title = {item.title}
+                 description = {item.description}
+                 image = {item.image}s
+                 onPress={() => console.log('Message selected',item)}
+                 renderRightActions={ () =>
+                                    <ListItemDeleteAction onPress={() => handelDelete(item)}/>
+                                    }
+
+                >
+                </ListItem>
+            }
+            ItemSeparatorComponent={ListItemSeparator}
+            ></FlatList>
+           
+        </Screen>
+    );
+}
+
+export default MessagesScreen;
+
+ /* function MessagesScreen(props) {
+ 
+
+
         <Screen>
         <FlatList
         data={messages}
         keyExtractor={message => message.id.toString()}
         renderItem={({item}) => 
-            <ListItem
-                title = {item.title}
-                description = {item.description}
-                image = {item.image}
+           <ListItem
+               title = {item.title}
+               description = {item.description}
+               image = {item.image}
                 onPress={() => console.log('Message selected',item)}
-            /> }
+                renderRightActions={() =>  (
+                <View style={{
+                        backgroundColor: 'red',
+                       width: 70,
+                       }}>
+                   </View>
+    )}
+            /> )}
             ItemSeparatorComponent={ListItemSeparator}
-        />
-    </Screen>
-    );
-}
-
-
-export default MessagesScreen;
+        
+    
+    
+     </Screen>
+     */
